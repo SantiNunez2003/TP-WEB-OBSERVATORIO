b992290cbc69c586b0a2e223ce07d0560b921916
Santiago Nuñez <133709203+SantiNunez2003@users.noreply.github.com>
2024-10-24 18:23:37 -0300

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -1,6 +1,9 @@
+CREATE DATABASE IF NOT EXISTS observatorio;
+USE observatorio;
+
 CREATE TABLE IF NOT EXISTS noticia (
   `id` TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id_imagen` TINYINT,
+  `url_imagen` TINYINT,
   `titulo` VARCHAR(255) NOT NULL,
   `descripcion` TINYTEXT NOT NULL,
   `fecha_publicacion` TIMESTAMP NOT NULL,
@@ -11,31 +14,27 @@ CREATE TABLE IF NOT EXISTS noticia (
 
 CREATE TABLE IF NOT EXISTS evento (
   `id` TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id_imagen` TINYINT,
+  `url_imagen` TINYINT,
   `nombre` VARCHAR(255) NOT NULL,
   `descripcion` TEXT NOT NULL,
   `horario` TIME NOT NULL,
   `fecha_evento` TIMESTAMP NOT NULL,
   `valor` DECIMAL(10, 2) NOT NULL,
-  `fehca_alta` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `fecha_alta` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `estatus` TINYINT(1)
 );
 
 CREATE TABLE IF NOT EXISTS imagen_galeria (
   `id` TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id_imagen` TINYINT,
+  `url_imagen` TINYINT,
   `fecha_alta` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `estatus` TINYINT(1)
 );
 
 CREATE TABLE IF NOT EXISTS imagenes (
   `id` TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `url` VARCHAR(255) NOT NULL,
+  `url_imagen` VARCHAR(255) NOT NULL,  -- Cambiado a url_imagen
   `nombre` VARCHAR(255) NOT NULL,
   `descripcion` TEXT,
   `fecha_subida` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE imagenes ADD CONSTRAINT imagenes_id_fk_noticia FOREIGN KEY (`id_imagen`) REFERENCES noticia (`id`);
-ALTER TABLE imagenes ADD CONSTRAINT imagenes_id_fk_imagen_galeria FOREIGN KEY (`id_imagen`) REFERENCES imagen_galeria (`id`);
-ALTER TABLE imagenes ADD CONSTRAINT imagenes_id_fk_evento FOREIGN KEY (`id_imagen`) REFERENCES evento (`id`);
