3789917fd15e1a346539cf1000c555e423dc9242
Santiago Nuñez <133709203+SantiNunez2003@users.noreply.github.com>
2024-10-24 19:53:10 -0300

    Create conexion.php

---
+++ php/conexion.php
@@ -0,0 +1,27 @@
+<?php
+function conectar()
+{
+	global $con;
+	$username = "";
+	$password = "";
+	$db = "";
+	$host = "";
+
+	$con = mysqli_connect($host, $username, $password, $db);
+	/* comprobar la conexión */
+	if (mysqli_connect_errno()) {
+		printf("Falló la conexión: %s\n", mysqli_connect_error());
+		exit();
+	} else {
+		$con->set_charset("utf8");
+		$ret = true;
+	}
+
+	return $ret;
+}
+function desconectar()
+{
+	global $con;
+	mysqli_close($con);
+}
+?>
