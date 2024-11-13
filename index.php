4e8c438bb316c2b3dd8d21263b83d23c53d3dd30
Santiago Nuñez <santiagon100@gmail.com>
2024-11-13 19:13:23 -0300

    Titulo updt

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php $titulo_modulo ?></title>
+    <title>Observatorio de las Misiones</title>
   
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
